e5f916932eb72ccabdc2a13ef147086325e8d77e
Diego José Saravia Zaldaña <saravia__2000@hotmail.com>
2020-04-24 21:59:04 -0500

    Starting 3er period first project

---
+++ 3er Corte/Juego_Teclado.X/main.c
@@ -0,0 +1,20 @@
+/*
+ * File:   main.c
+ * Author: user
+ *
+ * Created on 24 de abril de 2020, 09:54 PM
+ */
+
+
+#include <xc.h>
+
+#pragma config OSC = XT
+#pragma config PWRT = ON
+#pragma config BOR = OFF
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+#define _XTAL_FREQ 4000000
+
+void main(void) {
+    return;
+}
